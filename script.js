f4f3e62dcdf422bcc0f1fdf13ba7f18d328f06e8
Kevin-Y-3258 <yangk3258@my.sduhsd.net>
2026-03-02 21:50:07 +0000

    updated html

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // add javascript here
+document.getElementById("myBtn")?.addEventListener("Click", () => {
+    document.getElementById("my-offerings").toggleAttribute("hidden");
+})
+
+document.getElementById("questionBtn")?.addEventListener("click", () =>{
+    let question = document.getElementById("question");
+    let questionList = document.getElementById("questionList");
+    if(question.value.trim()!= ""){
+        const newQuestion = document.createElement("li");
+        newQuestion.textContent = question.value;
+        questionList.appendChild(newQuestion);
+        question.value = "";
+    }
+})
